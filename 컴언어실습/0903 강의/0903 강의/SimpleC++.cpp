0334a2f8dd4231d5dbdc48fe07491af13f6bcb58
ffvv0123 <63843822+ffvv0123@users.noreply.github.com>
2020-11-19 23:58:41 +0900

    Add files via upload

---
+++ 컴언어실습/0903 강의/0903 강의/SimpleC++.cpp
@@ -0,0 +1,14 @@
+/*
+	소스: SimpleC++.cpp
+cout과 << 연산자를 이용하여 화면에 출력한다.
+*/
+
+#include <iostream>
+//using namespace std;
+
+int main() {
+	std::cout << "";
+	std::cout << "";
+
+	return 0;
+}
---
+++ 컴언어실습/0903 강의/0903 강의/bool 데이터 타입.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	bool test = true;
+	cout << "1.bool: " << test << endl;
+
+	test = false;
+	cout << "2.bool: " << test << endl;
+
+	return 0;
+}
+
+// bool 데이터 값의 입력은 true / false로 입력.
+// 출력은 1 / 0 으로 표현
